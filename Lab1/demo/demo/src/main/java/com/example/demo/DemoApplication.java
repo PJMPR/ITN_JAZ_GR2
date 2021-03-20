d4c187ce842cbb1dfef6b55bc865373062d3d894
Adam <s15814@pjwstk.edu.pl>
2021-03-20 13:46:13 +0100

    Dodano wymagane klasy

--- Lab1/demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Lab1/demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 
-//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-//		context.scan("com.example.demo");
-//		context.refresh();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.example.demo");
+		context.refresh();
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-		ThirdComponent thirdComponent = context.getBean(ThirdComponent.class);
-		thirdComponent.printMe();
+		Person person = new Person("Adam");
+		Persondb db = context.getBean(Persondb.class);
+		PersonRepository repository = context.getBean(PersonRepository.class);
 	}
 
 }
---
+++ Lab1/demo/demo/src/main/java/com/example/demo/Persondb.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class Persondb {
+	ArrayList<Person> personArrayList;
+}
