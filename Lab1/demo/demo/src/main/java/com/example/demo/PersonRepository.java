b65879bb44e73b707569756a106aafdbc0873fcd
deohy <gorsham@gmail.com>
2021-03-21 22:42:37 +0100

    added classes

---
+++ Lab1/demo/demo/src/main/java/com/example/demo/PersonRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonRepository {
+    private PersonDb personDb;
+
+    public PersonRepository(PersonDb personDb) {
+        this.personDb = personDb;
+    }
+
+    public void save(Person person) {
+        personDb.addPersonToArray(person);
+    }
+
+    public PersonDb getDb() {
+        return personDb;
+    }
+}
