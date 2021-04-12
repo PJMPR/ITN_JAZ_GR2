d90f9f9b41ec52e013c23df33e600f89aa6dbe84
kethryll <angelslayer12@gmail.com>
2021-04-12 17:00:51 +0200

    added component import

--- Lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
+++ Lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +7,9 @@ public class DialogService {
 
     private final LoginMessages messages;
 
-    public DialogService(LoginMessages messages) {this.messages = messages;}
+    public DialogService(LoginMessages messages) {
+        this.messages = messages;
+    }
 
     public LoginMessages getLoginMessages() {return messages;}
 }
