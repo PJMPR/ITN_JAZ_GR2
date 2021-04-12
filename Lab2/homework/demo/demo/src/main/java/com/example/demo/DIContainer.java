f453ec31912d0a16ed3a2800616541d3624e2882
YOUR NAME <48855378+adrianszostak@users.noreply.github.com>
2021-04-12 23:27:00 +0200

    zadanie2

---
+++ Lab2/homework/demo/demo/src/main/java/com/example/demo/DIContainer.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class DIContainer {
+
+    @Bean
+    public DialogService dialogService(){
+        return new DialogService(loginMessages());
+    }
+
+    @Bean
+    public LoginMessages loginMessages(){
+        return new LoginMessages();
+    }
+}
---
+++ Lab2/homework/demo/demo/src/main/java/com/example/demo/LoginMessages.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class LoginMessages {
+    private String username;
+
+    @Value("${application.messages.welcome}")
+    private String welcomeMessage;
+
+    @Value("${application.messages.provideUsername}")
+    private String usernameMessage;
+
+    @Value("${application.messages.loggedAs}")
+    private String loggedAsMessage;
+
+
+    public String getUsernameMessage(){
+        return usernameMessage;
+    }
+
+    public String getLoggedAsMessage(){
+        return loggedAsMessage + " " + getUsername();
+    }
+
+    public String welcome() {
+        return welcomeMessage;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+}
