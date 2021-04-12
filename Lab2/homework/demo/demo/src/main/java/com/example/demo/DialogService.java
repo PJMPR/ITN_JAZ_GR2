00f5ec2fbabea63eb5b15d82aa7ee07a67bc4d63
kethryll <angelslayer12@gmail.com>
2021-04-12 16:44:33 +0200

    added classes and properties

--- Lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
+++ Lab2/homework/demo/demo/src/main/java/com/example/demo/DialogService.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-
+@Component
 public class DialogService {
+
+    private final LoginMessages messages;
+
+    public DialogService(LoginMessages messages) { this.messages = messages;}
+
+    public LoginMessages getLoginMessages() {return messages;}
 }
