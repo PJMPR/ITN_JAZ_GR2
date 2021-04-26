a7fcda17ef72e9a31eeda1d468f20b7b5f506b40
Sebastian Lewandowski <hulio90@gmail.com>
2021-04-26 12:05:23 +0200

    added my solution to task3

---
+++ Lab3/demo/demo/src/main/java/com/example/demo/controllers/PersonController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.Person;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("homework")
+public class PersonController {
+
+    @GetMapping("{path}")
+    public ResponseEntity getPathAndParam(@PathVariable("path") String path, @PathParam("query") String query) {
+        return ResponseEntity.ok("path:" + path + " query:" + query);
+    }
+
+    @PostMapping("person")
+    public ResponseEntity postPerson(@RequestBody Person person) {
+        return ResponseEntity.ok(person);
+    }
+
+    @PutMapping("person/{id}")
+    public ResponseEntity updatePerson(@PathVariable("id") String id, @RequestBody Person person) {
+        return ResponseEntity.ok(person);
+    }
+
+    @DeleteMapping("person/{id}")
+    public ResponseEntity deletePerson(@PathVariable("id") String id) {
+        return ResponseEntity.ok("");
+    }
+
+}
