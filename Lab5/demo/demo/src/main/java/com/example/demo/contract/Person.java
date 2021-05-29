d2c19ac3de200396decf3ec4a84087524adf82c4
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2021-05-29 13:06:00 +0200

    Lab5 end

--- Lab5/demo/demo/src/main/java/com/example/demo/contract/Person.java
+++ Lab5/demo/demo/src/main/java/com/example/demo/contract/Person.java
@@ -1,7 +1,15 @@
 package com.example.demo.contract;
 
+
+import javax.persistence.*;
+
+@Entity
+@NamedQuery(name = "Person.findByName",
+        query = "select p from Person p where p.name = ?1")
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int ID;
     String name;
     String surname;
--- Lab5/demo/demo/src/test/java/com/example/demo/CarApiTest.java
+++ Lab5/demo/demo/src/test/java/com/example/demo/CarApiTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -17,6 +18,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtests.properties")
 public class CarApiTest {
 
     @Autowired
