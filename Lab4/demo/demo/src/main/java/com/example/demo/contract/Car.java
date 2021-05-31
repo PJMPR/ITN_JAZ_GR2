541fe6e6bfea69efe3708f9f5f391bff800e4b87
carthe <32596055+carthe@users.noreply.github.com>
2021-05-31 22:45:44 +0200

    Add files via upload

---
+++ Lab4/demo/demo/src/main/java/com/example/demo/contract/Car.java
@@ -0,0 +1,58 @@
+package com.example.demo.contract;
+
+public class Car {
+
+    private String model;
+    private String registrationNumber;
+    private int milleage;
+    private boolean hasAccidents;
+    private double price;
+
+    public Car(String model, String registrationNumber, int milleage, boolean hasAccidents, double price) {
+        this.model = model;
+        this.registrationNumber = registrationNumber;
+        this.milleage = milleage;
+        this.hasAccidents = hasAccidents;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getMilleage() {
+        return milleage;
+    }
+
+    public void setMilleage(int milleage) {
+        this.milleage = milleage;
+    }
+
+    public boolean isHasAccidents() {
+        return hasAccidents;
+    }
+
+    public void setHasAccidents(boolean hasAccidents) {
+        this.hasAccidents = hasAccidents;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
