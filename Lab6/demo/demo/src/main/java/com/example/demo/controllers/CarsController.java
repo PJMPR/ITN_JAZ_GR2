a25a335b8675afd456633982aa3fa619b1c71d01
matrjx <jakub.prucnal@gmail.com>
2021-06-12 23:02:06 +0200

    AccidentsTest is ok.

--- Lab6/demo/demo/src/main/java/com/example/demo/controllers/CarsController.java
+++ Lab6/demo/demo/src/main/java/com/example/demo/controllers/CarsController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controllers;
 
-import com.example.demo.contract.Car;
+import com.example.demo.contract.AccidentDto;
+import com.example.demo.contract.CarDto;
+import com.example.demo.model.Accident;
+import com.example.demo.model.Car;
 import com.example.demo.services.CarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+
 import java.util.List;
 
 @RestController
@@ -20,8 +26,8 @@ public class CarsController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Car>> getAll() {
-        return ResponseEntity.ok(service.getAll());
+    public ResponseEntity getAll(@RequestParam(value = "model", required = false) String model) {
+        return ResponseEntity.ok(service.getAll(model));
     }
 
     @PostMapping()
@@ -31,23 +37,77 @@ public class CarsController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Car> getById(@PathVariable("id") int id) {
-        Car result = service.getById(id);
+    public ResponseEntity getById(@PathVariable("id") int id) {
+        CarDto result = service.getById(id);
         if (result == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(result);
     }
 
     @PutMapping("{id}")
     public ResponseEntity updateCar(@PathVariable("id") int id, @RequestBody Car car) {
-        Car result = service.update(id, car);
+        CarDto result = service.update(id, car);
         if (result == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity deleteCar(@PathVariable("id") int id) {
-        Car result = service.deleteCar(id);
+        CarDto result = service.deleteCar(id);
         if (result == null) return ResponseEntity.notFound().build();
         return ResponseEntity.noContent().build();
+    }
+
+    // /cars/1/accidents
+    @PostMapping("{id}/accidents")
+    public ResponseEntity addAccident(
+            @PathVariable("id") int id,
+            @RequestBody Accident accident
+    ){
+        AccidentDto a = service.saveAccident(id, accident);
+        if(a==null) return ResponseEntity.notFound().build();
+        UriComponents uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .pathSegment("cars")
+                .pathSegment(id+"")
+                .pathSegment("accidents")
+                .pathSegment(a.getId()+"").build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("{id}/accidents")
+    public ResponseEntity getAccidents(@PathVariable("id") int id){
+        List<AccidentDto> accidents = service.getAccidents(id);
+        if(accidents==null)return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(accidents);
+    }
+
+    @GetMapping("{id}/accidents/{accident_id}")
+    public ResponseEntity getAccidentById(
+            @PathVariable("id") int carId,
+            @PathVariable("accident_id") int accidentId
+    ){
+        AccidentDto accident = service.getAccidentById(carId, accidentId);
+        if(accident==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(accident);
+    }
+
+    @DeleteMapping("{id}/accidents/{accident_id}")
+    public ResponseEntity deleteAccident(
+            @PathVariable("id") int carId,
+            @PathVariable("accident_id") int accidentId
+    ){
+        AccidentDto accident = service.deleteAccident(carId, accidentId);
+        if(accident == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}/accidents/{accident_id}")
+    public ResponseEntity updateAccident(
+            @PathVariable("id") int carId,
+            @PathVariable("accident_id") int accidentId,
+            @RequestBody AccidentDto accident
+    ){
+        AccidentDto accidentDto = service.updateAccident(carId, accidentId, accident);
+        if(accidentDto==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().build();
     }
 }
