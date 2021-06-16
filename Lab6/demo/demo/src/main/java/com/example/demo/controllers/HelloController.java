271685cc4359700edd4297f0ed3cb686a12ae4ff
Sebastian Lewandowski <hulio90@gmail.com>
2021-06-16 18:59:20 +0200

    my solution for lab6

--- Lab6/demo/demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ Lab6/demo/demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import com.example.demo.contract.Car;
+import com.example.demo.model.Car;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
