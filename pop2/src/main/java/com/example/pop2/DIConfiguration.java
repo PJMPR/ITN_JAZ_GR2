fa43eef2b2072daee588a198c9f502bdf6d78728
matrjx <jakub.prucnal@gmail.com>
2021-06-26 20:13:06 +0200

    Second and Third exercise completed

--- pop2/src/main/java/com/example/pop2/DIConfiguration.java
+++ pop2/src/main/java/com/example/pop2/DIConfiguration.java
@@ -9,6 +9,9 @@ import java.util.List;
 @Configuration
 public class DIConfiguration {
 
+    @Value("${my.custom.property:To jest domyślna wiadomość}")
+    private String welcomeMessage;
+
     @Bean
     public CarPOJO car(){
         return new CarPOJO("","",100,false,0.00);
@@ -16,6 +19,8 @@ public class DIConfiguration {
 
     @Bean
     public List<String> defaultData(){
+
+        System.out.println(welcomeMessage);
         return List.of("String1", "String2", "String3", "String4", "String5");
     }
 }
